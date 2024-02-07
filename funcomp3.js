c88a7477f62821a196c36cf3c2b706a3857c7342
nithishareddy0804 <151110344+nithishareddy0804@users.noreply.github.com>
2024-02-07 21:19:02 +0530

    Add files via upload

---
+++ funcomp3.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Funcomp3(props){
+    return (<div>
+        <h1>function component 3</h1>
+        <h2>{props.name}</h2>
+    </div>)
+}
+export default Funcomp3;
