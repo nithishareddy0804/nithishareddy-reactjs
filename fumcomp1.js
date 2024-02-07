8af03dc3e91bdee8bb680ff2b70f4085cc011e41
nithishareddy0804 <151110344+nithishareddy0804@users.noreply.github.com>
2024-02-07 21:18:22 +0530

    Add files via upload

---
+++ fumcomp1.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Fumcomp1(props){
+    return (<div>
+        <h1>Function component 1</h1>
+        <h2>{props.name}</h2>
+    </div>)
+}
+export default Fumcomp1;
