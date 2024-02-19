12dea2e58dda4a6b9cfb8be40a5c02c3e6c7084b
nithishareddy0804 <151110344+nithishareddy0804@users.noreply.github.com>
2024-02-19 13:12:18 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Home = () => {
+
+  return (
+    <div>
+      <h3>Home</h3>
+      <h1 style={{ textAlign: "center", padding: "100px" }}>
+        Welcome To 10000 Coders
+      </h1>
+    </div>
+  );
+};
+export default Home;
