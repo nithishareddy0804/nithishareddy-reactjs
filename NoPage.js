b15eb649de576a1c0d7c21cd9ce990206d24f104
nithishareddy0804 <151110344+nithishareddy0804@users.noreply.github.com>
2024-02-19 13:13:22 +0530

    Add files via upload

---
+++ NoPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const NoPage = () => {
+  return( 
+  <div style={{textAlign:"center",color:"red",marginTop:"10%"}}>No Page Found</div>
+  )
+};
+export default NoPage;
